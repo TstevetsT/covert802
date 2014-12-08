2c908440021cf61bfb2cbe6a9525829778ed7883
TstevetsT <kaosnannaz@gmail.com>
2014-12-08 02:21:34 -0800

    Monday Morning 1

--- SAR.m
+++ SAR.m
@@ -1,42 +1,47 @@
+%% Modified code from:
+% http://www.mathworks.com/help/comm/gs/compute-ber-for-a-qam-system-with...
+% -awgn-using-matlab.html
 M = 4;                      % Size of signal constellation
-Mc = 16;                      % Size of signal constellation
+Mc = 16;                      % Size of covert constellation
 k = log2(M);                % Number of bits per symbol
-kc = log2(Mc);                % Number of bits per symbol
+kc = log2(Mc);                % Number of bits per covert symbol
 n = 3000;                  % Number of bits to process
 numSamplesPerSymbol = 1;    % Oversampling factor
 rng default                 % Use default random number generator
 dataIn = randi([0 1],n,1);  % Generate vector of binary data
-dataInc = randi([0 1],n,1);  % Generate vector of binary data
-%stem(dataIn(1:40),'filled');
-%title('Random Bits');
-%xlabel('Bit Index');
-%ylabel('Binary Value');
-dataInMatrix = reshape(dataIn,length(dataIn)/2,2)   % Reshape data into binary 4-tuples
+dataInc = randi([0 1],n,1);  % Generate vector of covert binary data
+dataInMatrix = reshape(dataIn,length(dataIn)/2,2)   % Reshape data into binary 2-tuples
 dataInMatrixc = reshape(dataInc,length(dataInc)/4,4)   % Reshape data into binary 4-tuples
 dataSymbolsIn = bi2de(dataInMatrix)                 % Convert to integers
 dataSymbolsInc = bi2de(dataInMatrixc)                 % Convert to integers
-%figure; % Create new figure window.
-%stem(dataSymbolsIn(1:10));
-%title('Random Symbols');
-%xlabel('Symbol Index');
-%ylabel('Integer Value');
 dataMod = qammod(dataSymbolsIn,M,0,'gray');  % Gray coding, phase offset = 0
 dataModc = qammod(dataSymbolsInc,Mc,0,'gray'); % Gray coding, phase offset = 0
-%for i=1:length(dataModc)
- %   if dataModc(i) == -1.0000 - 1.0000i
-  %      dataModc(i)=.5+.5i;
-   % elseif dataModc(i) == 1.0000 + 1.0000i
-    %    dataModc(i)=1.5+1.5i;
- %   elseif dataModc(i) == 1.0000 - 1.0000i
-  %      dataModc(i)=1.5+.5i;
-   % else dataModc(i)=.5+1.5i;
-    %end
-%end       
+dataModc=dataModc/2;       %Shrink constellation size to allign with signal
+dataModj=[dataMod;dataModc];    %Combine data from covert and cover signals
+dataSymbolsInc21=dataSymbolsInc(1:length(dataSymbolsInc)/2);
+dataSymbolsInc22=dataSymbolsInc(length(dataSymbolsInc)/2+1:end);
+dataModc21 = qammod(dataSymbolsInc21,Mc,0,'gray'); % Gray coding, phase offset = 0
+dataModc22 = qammod(dataSymbolsInc22,Mc,1,'gray'); % Gray coding, phase offset = 0
+dataModc2=[dataModc21;dataModc22];
+dataModc2=dataModc2/2;
+dataModj2=[dataMod;dataModc2];
 EbNo = 10;
 snr = EbNo + 10*log10(k) - 10*log10(numSamplesPerSymbol);
 receivedSignal = awgn(dataMod,snr,'measured');
 receivedSignalc = awgn(dataModc,snr,'measured');
-sPlotFig = scatterplot(receivedSignalc,1,0,'g.');
-%sPlotFig = scatterplot(dataModc,1,0,'g.');
+receivedSignalj = awgn(dataModj,snr,'measured');
+receivedSignalj2 = awgn(dataModj2,snr,'measured');
+sPlotFig = scatterplot(receivedSignal,1,0,'g.');
 hold on
 scatterplot(dataMod,1,0,'k*',sPlotFig)
+sPlotFigc = scatterplot(receivedSignalc,1,0,'g.');
+hold on
+scatterplot(dataModc,1,0,'k*',sPlotFigc)
+sPlotFigj = scatterplot(receivedSignalj,1,0,'g.');
+hold on
+scatterplot(dataModj,1,0,'k*',sPlotFigj)
+sPlotFigj2 = scatterplot(receivedSignalj2,1,0,'g.');
+hold on
+scatterplot(dataModj2,1,0,'k*',sPlotFigj2)
+dataSymbolsOutc = qamdemod(receivedSignalc, Mc);
+out=[dataSymbolsInc dataSymbolsOutc]
